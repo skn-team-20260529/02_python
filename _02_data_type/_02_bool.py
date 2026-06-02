b260a73e849032682501da27d56548bc7cb7f2d3
ujinkim2001 <ujinkim2001@naver.com>
2026-06-02 10:37:38 +0900

    20260529_데이터타입 (number, bool)

--- _02_data_type/_02_bool.py
+++ _02_data_type/_02_bool.py
@@ -71,3 +71,60 @@ print(score, type(score))
 result = score >= 60
 # print("합격 여부: ", result)
 print("합격 여부:", '합격' if result == True  else '불합격')
+
+
+
+
+
+# and 연산
+#- A 그리고 B
+#- A가 참이고 B도 참인 경우에 참
+
+#1+2 =>2항연산
+#T and T == T
+#T and F == F
+#F and T == F
+#F and F == F
+
+print('--- and ---')
+print(100>0 and 1==1)
+print(30>20  and 123 != 123)
+
+#a= ~~(계산식)
+#b= ~~(계산식)
+#print(a and b)   <==단순화됨
+
+
+#or 연산
+#A또는B가 T이면 결과도 T
+# <-> A와 B가 모두 F이면 F
+# T or T == T
+# T or F == T
+# F or T == T
+# F or F == F  (중요)
+
+print('--- or ---')
+print(100 > 0 or 1 == 1)
+print(10*10==100 or 1!=1)
+print(100==0 or 10 ==10)
+print(10+20*5==100 or 30/10+5==2)
+
+
+#60점 이상 입력 시 합격(T) 아니면 불합격(F)
+print("---합격/불합격---")
+score = input('점수를 입력하세요: ')
+print(score, type(score))
+
+
+#input() 함수: 키보드 입력을 받는 함수(str로 저장)
+#int() 함수: str->int로 변환
+score = int( input('점수를 입력하세요: ') )
+
+result = score >= 60
+print("합격 여부: ", result)
+
+
+
+#print("합격 여부: ", result)
+print("합격 여부: ", '합격' if result == True else '불합격')
+
