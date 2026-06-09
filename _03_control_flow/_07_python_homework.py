8e024b0145cbb7b96274d76d480e07893a49f460
ujinkim2001 <ujinkim2001@naver.com>
2026-06-09 18:14:16 +0900

    20260609_공부

---
+++ _03_control_flow/_07_python_homework.py
@@ -0,0 +1,86 @@
+# [1] 변수
+#문제1
+age = 32
+name = '김유진'
+print(age)
+print(name)
+
+#문제2
+height = 167
+dream = '콘텐츠 ai 전문가'
+print(height)
+print(dream)
+
+#문제3
+city = '서울시'
+my_city = city
+print(city)
+print(my_city)
+
+#문제4
+apple_count = 10
+orange_count = 20
+print(apple_count)
+print(orange_count)
+
+#문제5
+first_name = "yoojin"
+last_name = "kim"
+full_name = first_name + last_name
+print(full_name)
+
+
+#[2] 기본 자료형
+#문제1
+num1 = 10
+num2 = 5
+sum_result = num1 + num2
+print(sum_result)
+
+#문제2
+x=7.5
+y=2
+result = x / y
+print(result)
+
+#문제3
+is_student = True
+print(is_student, type(is_student))
+
+#문제4  [X]
+num1 = 15
+num2=4
+result2 = num1 / num2
+print(result2)
+#▼
+num1 = 15
+num2 = 4
+remainder = num1 % num2
+quotient = num1 // num2
+print(quotient)  #몫
+print(remainder) #나머지
+
+#문제5
+greeting_message = "Hello"
+greeting_target = "World"
+print(greeting_message + greeting_target)
+
+#문제6
+base = 2
+exponent = 5
+result = base ** exponent
+print(result)
+
+#문제7
+number = 3
+result3 = number % 2
+print(result3)
+
+#문제8
+age = 32
+city = "서울시"
+print(f'나는 {age}살이고, {city}에 살고 있습니다.')
+
+
+#[3] 리스트 & 튜플
+#문제1
